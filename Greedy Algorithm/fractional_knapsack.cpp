b7899601fc8e9e27cc8e064dc1778093dba33e52
Chintan <chintansutariya322@gmail.com>
2022-01-10 15:12:49 -0500

    fractional knapsack problem code addeed

---
+++ Greedy Algorithm/fractional_knapsack.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool compare(pair<int, int> a, pair<int, int> b){
+    double v1 = (double)a.first/a.second;
+    double v2 = (double)b.first/b.second;
+
+    return v1>v2;
+}
+
+int  main(){
+    int n;
+    cin>>n;
+
+    vector<pair<int,int>> a(n);
+
+    for(int i=0; i<n; i++){
+        cin>>a[i].first>> a[i].second;
+    }
+    int w;
+    cin>>w;
+
+    sort(a.begin(), a.end(), compare);
+
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        if(w >= a[i].second){
+            ans += a[i].first;
+            w -= a[i].second;
+            continue;
+        }
+
+        double vw = (double) a[i].first/a[i].second;
+        ans += vw * w;
+        w = 0;
+        break;
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+}
