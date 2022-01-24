b282cfb7e9bcec99d5adb03ac702710e010faa5d
Chintan <chintansutariya322@gmail.com>
2022-01-24 16:24:28 -0500

    LCS in 3 strings code updated using DP

--- Dynamic Prgramming/LCS_in_3_strings.cpp
+++ Dynamic Prgramming/LCS_in_3_strings.cpp
@@ -12,22 +12,27 @@ using namespace std;
 
 const int N = 1e5+2, MOD = 1e9+7;
 
+int dp[205][205][205];
+
 int LCS(string &s1,string &s2,string &s3,int i,int j,int k) {
 
     if(i == 0 || j == 0 || k == 0) return 0;
 
+    if(dp[i][j][k] != -1) return dp[i][j][k];
+
     if(s1[i-1] == s2[j-1] && s2[j-1] == s3[k-1]){
-        return 1 + LCS(s1,s2,s3,i-1,j-1,k-1);
+        return dp[i][j][k] = 1 + LCS(s1,s2,s3,i-1,j-1,k-1);
     }else{
         int l = LCS(s1,s2,s3,i-1,j,k);
         int r = LCS(s1,s2,s3,i,j-1,k-1);
         int p = LCS(s1,s2,s3,i,j,k-1);
-        return max({l,r,p});
+        return dp[i][j][k] = max({l,r,p});
     }
 }
 
 int main(){
     
+    memset(dp,-1,sizeof(dp));
 
     string s1,s2,s3;
     cin>>s1>>s2>>s3;
